a888ab194b0f2a5447feaf0c9229f8f788db6798
권형석 <hskwon517@gmail.com>
2025-04-16 09:32:34 +0900

    chore: React Query Provider 파일 추가 및 layout에 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
+
 import { Inter } from "next/font/google";
+
+import { TanstackQueryProvider } from "@/lib/tanstack-provider";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <TanstackQueryProvider>
+        <body className={inter.className}>{children}</body>
+      </TanstackQueryProvider>
     </html>
   );
 }
