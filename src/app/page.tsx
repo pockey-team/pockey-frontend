41958e70480226b001a6d91a8388732a67b2e9f9
권형석 <hskwon517@gmail.com>
2025-04-16 09:52:55 +0900

    chore: 프론트엔드 개발자의 동일한 개발 환경 구축을 위한 config 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
-  return (
-    <main>
-      <h1 className="text-4xl font-bold tracking-tight">Pockey Frontend</h1>
-    </main>
-  );
+    return (
+        <main>
+            <h1 className="flex text-4xl font-bold tracking-tight">Pockey Frontend</h1>
+        </main>
+    )
 }
