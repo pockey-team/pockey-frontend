3328b4f3b6bc83329e5ea0329f41195ddbf88b60
권형석 <hskwon517@gmail.com>
2025-04-24 15:46:29 +0900

    feat: Next Auth 관련 middleware 파일 생성

---
+++ src/lib/auth.ts
@@ -0,0 +1,34 @@
+import { NextAuthOptions } from "next-auth";
+import KakaoProvider from "next-auth/providers/kakao";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID!,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent"
+        }
+      }
+    })
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+
+  callbacks: {
+    async signIn({ account }) {
+      return account?.provider === "kakao";
+    },
+    async redirect({ url, baseUrl }) {
+      if (url.startsWith(baseUrl)) {
+        return url;
+      }
+      return baseUrl;
+    }
+  },
+
+  pages: {
+    signIn: "/auth/signIn",
+    error: "/auth/error"
+  }
+};
---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+
+import { withAuth } from "next-auth/middleware";
+
+export default withAuth(
+  function middleware() {
+    return NextResponse.next();
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+
+    pages: {
+      signIn: "/auth/signIn",
+      error: "/auth/error",
+    }
+  }
+);
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|auth|$).*)" ],
+};
