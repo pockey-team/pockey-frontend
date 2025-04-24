462ebf2e2c05b1009febdee2da397906bdbfd4ef
권형석 <hskwon517@gmail.com>
2025-04-24 15:51:48 +0900

    chore: 메인 Page 스타일 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
-export default function Home() {
+import Link from "next/link";
+import { getServerSession } from "next-auth";
+
+import { authOptions } from "@/lib/auth";
+
+import { Button } from "@/components/ui/button";
+import { SignOutButton } from "@/components/auth/sign-out-button";
+
+export const Home = async () => {
+  const session = await getServerSession(authOptions);
   return (
-    <main>
+    <main className="flex flex-col items-center gap-4">
       <h1 className="text-4xl font-bold tracking-tight">Pockey Frontend</h1>
+
+      {/* 추후 제거 예정입니다. */}
+      {!session && (
+        <Button asChild>
+          <Link href="/auth/signIn">로그인 페이지로 이동</Link>
+        </Button>
+      )}
+
+      {/* 추후 제거 예정입니다. */}
+      {session && (
+        <SignOutButton />
+      )}
     </main>
   );
-}
+};
+
+export default Home;
