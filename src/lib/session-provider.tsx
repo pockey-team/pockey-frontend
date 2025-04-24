9e16348ef12786f5a59357474206ca22fb35f4e5
권형석 <hskwon517@gmail.com>
2025-04-24 15:51:29 +0900

    feat: SessionProvider 컴포넌트 추가

---
+++ src/lib/session-provider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+
+type AuthSessionProps = ({
+  children: React.ReactNode;
+});
+
+export const AuthSession = ({
+  children
+}: AuthSessionProps) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
