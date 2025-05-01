2a89563f06c83b9fa926ff8c7b44367a60512ffc
권형석 <hskwon517@gmail.com>
2025-05-01 12:25:06 +0900

    feat: getPresents api 호출 함수 추가

---
+++ src/api/Present/get-presents.ts
@@ -0,0 +1,21 @@
+import { PRESENTS, Present } from "@/constants/Presents";
+
+export const getPresents = async (options?: {
+  delay?: number;
+  empty?: boolean;
+  error?: boolean;
+}): Promise<Present[]> => {
+  const { delay = 1000, empty = false, error = false } = options || {};
+
+  await new Promise((resolve) => setTimeout(resolve, delay));
+
+  if (error) {
+    throw new Error("선물 데이터를 불러오는데 실패했습니다.");
+  }
+
+  if (empty) {
+    return [];
+  }
+
+  return PRESENTS;
+};
