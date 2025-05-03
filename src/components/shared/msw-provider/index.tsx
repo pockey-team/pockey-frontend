e6a66331403d2b412081499167cf6823884155b8
sudosubin <sudosubin@gmail.com>
2025-05-04 01:41:34 +0900

    refactor: Provider 리팩토링, 폰트 리팩토링

--- src/components/shared/msw-provider/index.tsx
+++ src/components/shared/msw-provider/index.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { PropsWithChildren, Suspense, use } from "react";
+import { Fragment, PropsWithChildren, Suspense, use } from "react";
 
 const setupMSW =
-  process.env.NODE_ENV === "development" && typeof window !== "undefined"
-    ? import("@/mocks/browser").then(async ({ worker }) => {
+  typeof window === "undefined"
+    ? Promise.resolve()
+    : import("@/mocks/browser").then(async ({ worker }) => {
         await worker.start({
           onUnhandledRequest(request, print) {
             if (request.url.includes("_next")) {
@@ -13,20 +14,18 @@ const setupMSW =
             print.warning();
           },
         });
-      })
-    : Promise.resolve();
+      });
 
-export const MSWProvider = ({ children }: PropsWithChildren) => {
-  if (process.env.NODE_ENV !== "development") {
-    return children;
-  }
-
-  return (
-    <Suspense fallback={null}>
-      <MSWProviderInner>{children}</MSWProviderInner>
-    </Suspense>
-  );
-};
+export const MSWProvider =
+  process.env.NODE_ENV === "production"
+    ? Fragment
+    : ({ children }: PropsWithChildren) => {
+        return (
+          <Suspense fallback={null}>
+            <MSWProviderInner>{children}</MSWProviderInner>
+          </Suspense>
+        );
+      };
 
 const MSWProviderInner = ({ children }: PropsWithChildren) => {
   use(setupMSW);
