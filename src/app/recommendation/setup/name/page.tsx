92d3595c9ca1b3a3ef6bcabe2e3266e771ea21bf
sudosubin <sudosubin@gmail.com>
2025-05-14 01:24:07 +0900

    feat: 추천 이름 설정 페이지 구현

--- src/app/recommendation/setup/name/page.tsx
+++ src/app/recommendation/setup/name/page.tsx
@@ -1,44 +1,68 @@
 "use client";
 
-import { Search } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { BottomBar } from "@/components/layout/bottom-bar";
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/shared/button";
 import { Page } from "@/components/shared/page";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function RecommendationSetupNamePage() {
+  const router = useRouter();
+
   return (
-    <Page className="bg-[linear-gradient(155deg,_#6299ff_44%,_#a6d6ff_93%)]">
+    <Page className="bg-gray-800">
       <Page.Header>
-        <Page.Header.Left>
-          <Link href="/">
-            <Image src="/logo.svg" alt="logo" width={100} height={100} />
-          </Link>
-        </Page.Header.Left>
         <Page.Header.Right>
-          <Link href="/">
-            <Search className="text-white" />
+          <Link href="/" className="text-body-16-regular text-gray-500">
+            닫기
           </Link>
         </Page.Header.Right>
       </Page.Header>
 
-      <Page.Container className="flex flex-1 flex-col items-center justify-center pb-[100px]">
-        <div className="mb-[60px] rounded-full bg-white px-[10px] py-[8px] text-[#4282F9] text-body-16-bold">
-          필수 정보 4개
-        </div>
-
-        <Page.Title className="mb-16px font-bold text-white">
-          누구에게 선물하고자 하나요?
+      <Page.Container className="flex flex-1 flex-col justify-center pb-[100px]">
+        <Page.Title className="text-gray-100">
+          누구에게 줄 선물인가요?
         </Page.Title>
 
-        <Page.SubTitle className="text-gray-100">
-          선물하고자 하는 상대에 대해 알려주세요
-        </Page.SubTitle>
+        <div className="flex w-full items-center justify-center px-20px py-[28px]">
+          <Present21Image />
+        </div>
+
+        <div className="flex flex-col px-[27px]">
+          <Label htmlFor="name">선물 받는 사람</Label>
+          <Input name="name" placeholder="이름을 지어주세요" maxLength={7} />
+        </div>
       </Page.Container>
 
       <Page.ActionButton>
-        {(props) => <BottomBar {...props} />}
+        {(props) => (
+          <Button
+            {...props}
+            size="x-large"
+            aria-selected={true}
+            onClick={() => router.push("/recommendation/setup/steps/1")}
+          >
+            다음
+          </Button>
+        )}
       </Page.ActionButton>
     </Page>
   );
 }
+
+const Present21Image = () => {
+  return (
+    <div className="relative flex items-center justify-center">
+      <div className="absolute h-[180px] w-[180px] rounded-full bg-[#94F2FF]/[.68] blur-[100px]" />
+      <Image
+        src="/static/images/recommendation-present-21.png"
+        alt="선물 상자"
+        width={632}
+        height={631}
+        className="relative z-10"
+      />
+    </div>
+  );
+};
