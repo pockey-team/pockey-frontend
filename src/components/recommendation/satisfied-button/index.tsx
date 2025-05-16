f794657cd11c50955fdbca3603056df078a6d624
권형석 <hskwon517@gmail.com>
2025-05-16 11:14:35 +0900

    feat: 클릭 시 session 여부에 따라 handler 함수 내 분기 처리

--- src/components/recommendation/satisfied-button/index.tsx
+++ src/components/recommendation/satisfied-button/index.tsx
@@ -1,46 +1,51 @@
 "use client";
 
-import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 import { useState } from "react";
+import { SignInButton } from "@/components/auth/sign-in";
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
-  DialogFooter,
+  DialogDescription,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 
 export const SatisfiedButton = () => {
+  const router = useRouter();
   const [isOpen, setIsOpen] = useState(false);
+  const session = useSession();
+
+  const handleClick = () => {
+    if (session.data) {
+      router.push(`/recommendation/result/${"상품디테일아이디"}`);
+    } else {
+      setIsOpen(true);
+    }
+  };
 
   return (
     <Dialog open={isOpen} onOpenChange={setIsOpen}>
-      <DialogTrigger asChild>
-        <Button
-          variant="outline"
-          className="w-1/2 rounded-xl bg-primary-500 py-16px text-subtitle-18-bold text-xl tracking-tight hover:bg-[#C0DAFF]/80 focus:bg-primary-500 focus-visible:bg-primary-500 active:bg-primary-500"
-        >
-          마음에 들어요
-        </Button>
-      </DialogTrigger>
-      <DialogContent className="min-h-[258px] w-full max-w-[295px] rounded-xl border-none bg-gray-700 text-white">
-        <DialogHeader>
-          <DialogTitle className="sr-only">
-            로그인 하고 추천 이유를 확인하세요
+      <Button
+        onClick={handleClick}
+        variant="outline"
+        className="w-1/2 rounded-xl bg-primary-500 py-16px text-subtitle-18-bold text-xl tracking-tight hover:bg-[#C0DAFF]/80 focus:bg-primary-500 focus-visible:bg-primary-500 active:bg-primary-500"
+      >
+        마음에 들어요
+      </Button>
+      <DialogContent className="min-h-[276px] w-[310px] rounded-2xl border-none bg-gray-800 p-24px">
+        <DialogHeader className="flex size-full flex-col justify-center gap-24px">
+          <DialogTitle className="text-gray-100 text-heading-24-semibold">
+            추천 이유가 궁금하다면 <br />
+            지금 로그인해보기
           </DialogTitle>
+          <DialogDescription className="text-body-14-semibold">
+            선택된 취향과 관심사를 바탕으로 골랐어요.
+          </DialogDescription>
         </DialogHeader>
-        <h2 className="text-center text-heading-24-semibold">
-          <span>로그인 하고</span> <br />
-          <span>추천 이유를 확인하세요</span>
-        </h2>
-        <DialogFooter className="flex flex-row items-center justify-center">
-          {/* TODO. signIn 버튼 컴포넌트로 변경 */}
-          <Button className="bg-[#4c4c4c] p-16px text-white" asChild>
-            <Link href="/login">카카오 로그인하러 가기</Link>
-          </Button>
-        </DialogFooter>
+        <SignInButton />
       </DialogContent>
     </Dialog>
   );
