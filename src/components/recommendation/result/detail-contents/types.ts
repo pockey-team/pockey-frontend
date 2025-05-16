e03e93be00f11313eab334448f490237b14a7176
권형석 <hskwon517@gmail.com>
2025-05-16 16:27:56 +0900

    feat: 상품 추천 상세 페이지

---
+++ src/components/recommendation/result/detail-contents/types.ts
@@ -0,0 +1,16 @@
+export interface RecommendationDetailContentsProps {
+  showCategory?: boolean;
+  showHeart?: boolean;
+  showFeelingsSection?: boolean;
+  showMessageSection?: boolean;
+  showRelatedProductsSection?: boolean;
+  productImage?: string;
+  productTitle?: string;
+  priceRange?: string;
+  category1?: string;
+  category2?: string;
+  feelings?: string[];
+  messageTitle?: string;
+  messageContent?: string;
+  relatedProducts?: Array<{ id: number; title: string; imageUrl: string }>;
+}
