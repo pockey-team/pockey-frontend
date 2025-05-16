b53cab0d2f58ac83e83e74eec4d00aead71e2218
권형석 <hskwon517@gmail.com>
2025-05-16 09:24:02 +0900

    feat: protected route 정의 및 해당할경우 리다이렉트 처리

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,25 @@ import { NextResponse } from "next/server";
 
 import { withAuth } from "next-auth/middleware";
 
+const protectedRoutes = ["/recommendation/result"];
+
+const isProtectedRoute = (path: string) => {
+  return protectedRoutes.some((route) => path.startsWith(route));
+};
+
 export default withAuth(
   function middleware() {
     return NextResponse.next();
   },
   {
     callbacks: {
-      authorized: ({ token }) => !!token,
+      authorized: ({ token, req }) => {
+        if (isProtectedRoute(req.nextUrl.pathname)) {
+          return !!token;
+        }
+
+        return true;
+      },
     },
 
     pages: {
