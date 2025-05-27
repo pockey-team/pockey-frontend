75dc9ef83d612428f815a301a2b609f3546e617b
권형석 <hskwon517@gmail.com>
2025-05-27 16:46:28 +0900

    fix: isSharePage 프롭스에 따른 스타일 분기 처리

--- src/components/recommendation/result/detail-contents/content-section.tsx
+++ src/components/recommendation/result/detail-contents/content-section.tsx
@@ -8,6 +8,7 @@ interface Props {
   isCapturing?: boolean;
   showBackground?: boolean;
   hideOnCapture?: boolean;
+  isSharePage?: boolean;
 }
 
 export const ContentSection = ({
@@ -18,24 +19,27 @@ export const ContentSection = ({
   isCapturing = false,
   showBackground = false,
   hideOnCapture = false,
+  isSharePage = false,
 }: Props) => {
   return (
     <section
       className={cn(
-        "my-24px rounded-3xl py-16px",
+        "rounded-3xl py-16px",
         hideOnCapture ? "hidden" : "block",
-        isCapturing
-          ? "my-0px bg-[#E5E7EB]"
-          : showBackground
-            ? "bg-gray-800 p-8px"
-            : "!my-0px",
+        isSharePage ? "my-8px" : "my-24px",
+        {
+          "my-0px bg-[#E5E7EB]": isCapturing,
+          "!my-0px": !showBackground,
+          [`p-16px ${isSharePage ? "bg-[#E5E7EBA1]" : "bg-gray-800"}`]:
+            showBackground && !isCapturing,
+        },
         className,
       )}
     >
       <h2
         className={cn(
-          "mb-12px text-subtitle-18-bold",
-          isCapturing ? "text-gray-900" : "text-gray-100",
+          "mb-12px text-body-14-semibold",
+          isCapturing || isSharePage ? "text-gray-700" : "text-gray-100",
         )}
       >
         {title}
@@ -43,8 +47,8 @@ export const ContentSection = ({
       {subTitle && (
         <p
           className={cn(
-            "mb-12px text-body-16-regular",
-            isCapturing ? "text-[#407CFF]" : "text-primary-500",
+            "mb-12px text-body-16-bold",
+            isCapturing || isSharePage ? "text-[#407CFF]" : "text-primary-500",
           )}
         >
           {subTitle}
