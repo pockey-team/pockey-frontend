f144596bf10ff5c1a060416100a7adf5d4bca59c
권형석 <hskwon517@gmail.com>
2025-05-30 10:12:06 +0900

    fix: 버튼 scale 애니메이션 추가

--- src/components/auth/sign-in/index.tsx
+++ src/components/auth/sign-in/index.tsx
@@ -3,6 +3,7 @@
 import { useSearchParams } from "next/navigation";
 import { Button } from "@/components/shared/button";
 import { useKakaoSignIn } from "@/hooks/useKakaoSignIn";
+import { cn } from "@/lib/utils";
 
 interface Props {
   callback?: string;
@@ -17,7 +18,10 @@ export const SignInButton = ({ callback }: Props) => {
     <Button
       size="large"
       onClick={login}
-      className="hover:!bg-kakao/80 h-56px w-full min-w-40 border-none bg-kakao font-bold text-gray-700 text-subtitle-18-bold text-yellow-900 tracking-tight shadow-sm"
+      className={cn(
+        "hover:!bg-kakao/80 h-56px w-full min-w-40 border-none bg-kakao font-bold text-gray-700 text-subtitle-18-bold text-yellow-900 tracking-tight shadow-sm",
+        "scale-100 active:scale-95 active:transition-all active:duration-300 active:ease-in-out",
+      )}
     >
       카카오로 시작하기
     </Button>
--- src/components/recommendation/result/present-recommendation-result/action-buttons.tsx
+++ src/components/recommendation/result/present-recommendation-result/action-buttons.tsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 import { SatisfiedButton } from "@/components/recommendation/satisfied-button";
 import { Page } from "@/components/shared/page";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
 interface ActionButtonsProps {
   nextPickCount: number;
@@ -38,7 +39,10 @@ export const ActionButtons = ({
             disabled={nextPickCount === 0}
             onClick={onNextResult}
             variant="ghost"
-            className="w-1/2 rounded-xl bg-gray-700 py-16px text-gray-500 text-subtitle-18-bold text-xl tracking-tight hover:bg-gray-700/80 hover:text-gray-500 focus:bg-gray-700 focus:text-gray-500 active:bg-gray-700 active:text-gray-500"
+            className={cn(
+              "w-1/2 rounded-xl bg-gray-700 py-16px text-gray-500 text-subtitle-18-bold text-xl tracking-tight hover:bg-gray-700/80 hover:text-gray-500 focus:bg-gray-700 focus:text-gray-500 active:bg-gray-700 active:text-gray-500",
+              "scale-100 active:scale-95 active:transition-all active:duration-300 active:ease-in-out",
+            )}
           >
             별로예요
           </Button>
